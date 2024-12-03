da0fac6578fb9f3a5a8de2fe8ca48ba2685f599d
dapachy <patgross2411@gmail.com>
2024-12-03 18:15:47 +0100

    added vial config,keymap,rules,json for ymdk/ymd40/v2

---
+++ keyboards/ymdk/ymd40/v2/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x11, 0xD7, 0x00, 0xAA, 0xC6, 0x8E, 0xAB, 0xCE}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+
